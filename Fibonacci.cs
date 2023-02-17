41dd1f56f6b1226481fcb8bc5f5a3558c4a707ec
Tech with Linh <111438190+tech-linh@users.noreply.github.com>
2023-02-17 16:57:20 +0100

    Update Fibonacci.cs

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -23,3 +23,57 @@ namespace Fibonacci
         }
     }
 }
+
+
+/*Write a Fibonacci exercise in C#.
+The Fibonacci sequence is a series of numbers where a number is the sum of the two preceding numbers.
+The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+This is a program that takes an integer and prints out the Fibonacci sequence leading up to that number.
+For example, if the input is 8, your program should output 1, 1, 2, 3, 5.
+If the input is 20, the output should be 1, 1, 2, 3, 5, 8, 13.
+If the input is 1, the output should just be “1”.*/
+
+using System;
+
+namespace RandomTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Initialize variables for the first and second numbers in the sequence, and the sum
+            int firstNumber = 1;
+            int secondNumber = 1;
+            int sum = 0;
+
+            // Print the first two numbers in the sequence
+            Console.Write(firstNumber + " " + secondNumber + " ");
+
+            // Generate the Fibonacci sequence up to the entered number
+            while (sum < number)
+            {
+                sum = firstNumber + secondNumber;
+
+                // Update the values of the first and second numbers in the sequence
+                firstNumber = secondNumber;
+                secondNumber = sum;
+
+                // Print the sum if it is less than the entered number
+                if (sum < number)
+                {
+                    Console.Write(sum + " ");
+                }
+
+                //Print the current number in the sequence without a trailing space if it equals the entered number
+                else
+                {
+                    Console.Write(sum);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
