4f0c4386179c63f3aa75fef09c9c619d9eda6fc9
Tech with Linh <111438190+tech-linh@users.noreply.github.com>
2023-02-19 12:17:47 +0100

    Update %3 && %7.cs

--- %3 && %7.cs
+++ %3 && %7.cs
@@ -2,7 +2,7 @@
 
 while (true)
 {
-    Console.WriteLine("Enter a positive number to check whether it's a multiple of 3 or 7: ");
+    Console.WriteLine("Enter a positive number to check whether if it's a multiple of 3 or 7: ");
     int x = Convert.ToInt32(Console.ReadLine());
 
     var y = (x % 3 == 0) || (x % 7 == 0);
